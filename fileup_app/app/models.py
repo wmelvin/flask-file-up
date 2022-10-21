da606ece33656999738f4042ac2b40bb0c1cf304
Bill Melvin <bill@billmelvin.com>
2022-10-21 09:38:23 -0400

    Add organization (Org) to model.

--- fileup_app/app/models.py
+++ fileup_app/app/models.py
@@ -3,14 +3,27 @@ from datetime import datetime
 from app import db
 
 
+class Org(db.Model):
+    __tablename__ = "orgs"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    orgname = db.Column(db.String(64), nullable=False, unique=True)
+    when_added = db.Column(db.DateTime, nullable=False, default=datetime.now)
+
+    def __repr__(self) -> str:
+        return f"<Org {self.id}>"
+
+
 class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String, nullable=False, unique=True)
-    email = db.Column(db.String, nullable=False, unique=True)
-    password_hash = db.Column(db.String, nullable=False)
+    org_id = db.Column(db.Integer, db.ForeignKey("orgs.id"), nullable=False)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    email = db.Column(db.String(80), nullable=False, unique=True)
+    password_hash = db.Column(db.String(255), nullable=False)
     when_added = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    is_active = db.Column(db.Boolean, default=False)
 
     def __repr__(self) -> str:
         return f"<User {self.id}>"
@@ -21,9 +34,14 @@ class UploadedFile(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     file_name = db.Column(db.String(255), nullable=False)
+    org_id = db.Column(db.Integer, db.ForeignKey("orgs.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     when_uploaded = db.Column(
         db.DateTime, nullable=False, default=datetime.now
     )
 
     def __repr__(self) -> str:
         return f"<UploadedFile {self.id}>"
+
+    # TODO: Only active users should be able to see uploads.
+    # The organization owns the uploaded file.
