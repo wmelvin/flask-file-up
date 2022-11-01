9d1d6f147162578aeff39a232d235ec9b1b0b333
Bill Melvin <bill@billmelvin.com>
2022-11-01 14:29:09 -0400

    Work on script for creating Azure resources.

--- create-azure-resources.ps1
+++ create-azure-resources.ps1
@@ -1,8 +1,17 @@
-# ----------------------------------------------------------------------
-# PowerShell script with steps to deploy using the Azure CLI.
+# ======================================================================
+#  PowerShell script with steps to deploy using the Azure CLI.
 #
-# This script is intended to be executed in selected sections (F8 in IDE
-# or paste into CLI), not all at once.
+#  Some commented-out commands in this script are intended to be executed
+#  separately using F8 in an IDE (or by pasting into a shell to run).
+#
+#  Running this script will:
+#  - Create a Resource Group.
+#  - Create an App Service Plan (Linux).
+#  - Create a Web App.
+#  - Configure the Web App for ZIP file deployment.
+#  - Create SQL Server.
+#  - Add firewall rules to the SQL Server.
+#  - Create a Database on the SQL Server.
 #
 # ----------------------------------------------------------------------
 
@@ -10,29 +19,41 @@
 
 # az account set -s $SUBSCRIPTION
 
+# ----------------------------------------------------------------------
+# Script initialization:
 
 $baseName = "fileup"
-
 
 # -- Get key variables from file in local encrypted folder.
 
 $keysFile = "$env:UserProfile\KeepLocal\${baseName}-settings.ps1"
 
+# -- The file must set these variables:
+#  $sqlAdminUser = ""
+#  $sqlAdminPass = ""
+#  $appSecretKey = ""
+#  $appMaxUploadSizeMb = ""
+
 # -- Source the file to set the vars.
 . $keysFile
 
-if (0 -eq $SqlAdminUser.Length) {
-    Write-Host "Failed to get SqlAdminUser from '$keysFile'."
-    Exit 1
+# -- Check that the required variables were set.
+function CheckVarSet ([string] $varName) {
+    $val = Get-Variable -Name $varName -ValueOnly -ErrorAction:Ignore
+    if (0 -eq $val.Length) {
+      Write-Host "ERROR: '$varName' not set in '$keysFile'."
+      Exit 1
+    }
 }
 
-if (0 -eq $SqlAdminPass.Length) {
-  Write-Host "Failed to get SqlAdminPass from '$keysFile'."
-  Exit 1
-}
+CheckVarSet "sqlAdminUser"
+CheckVarSet "sqlAdminPass"
+CheckVarSet "appSecretKey"
+CheckVarSet "appMaxUploadSizeMb"
 
 
-# -- Assign vars for script.
+# -- Assign additional variables used in this script.
+
 $rgName = "${baseName}-rg"
 $location = "eastus"
 $uniqtag = "31"
@@ -42,8 +63,14 @@ $webAppName = "${baseName}${uniqtag}webapp"
 $sqlServerName = "${baseName}${uniqtag}dbsrv"
 $sqlDatabaseName = "${baseName}${uniqtag}sqldb"
 
+$appDatabaseURI = "mssql+pyodbc://${SqlAdminUser}:${SqlAdminPass}@${slqServerName}:1433/${sqlDatabaseName}?driver=ODBC+Driver+18+for+SQL+Server"
 
-# -- Create the Resource Group.
+
+# ======================================================================
+# Create and configure Azure resources.
+
+Write-Host "`nSTEP - Creating resource group: $rgName`n"
+
 az group create -n $rgName -l $location
 
 
@@ -59,6 +86,8 @@ az group create -n $rgName -l $location
 # -- Create the App Service Plan (Linux).
 #    https://docs.microsoft.com/en-us/cli/azure/appservice/plan?view=azure-cli-latest#az-appservice-plan-create
 
+Write-Host "`nSTEP - Creating App Service Plan: $appServiceName`n"
+
 az appservice plan create `
   --name $appServiceName `
   --resource-group $rgName `
@@ -72,6 +101,8 @@ az appservice plan create `
 #
 #    az webapp list-runtimes
 
+Write-Host "`nSTEP - Creating Web App: $webAppName`n"
+
 az webapp create `
   -g $rgName `
   -p $appServiceName `
@@ -79,8 +110,33 @@ az webapp create `
   --runtime "PYTHON:3.10"
 
 
+# -- Configure for ZIP file deployment.
+#    https://learn.microsoft.com/en-us/azure/app-service/quickstart-python?tabs=flask%2Cwindows%2Cazure-cli%2Czip-deploy%2Cdeploy-instructions-azcli%2Cterminal-bash%2Cdeploy-instructions-zip-azcli#3---deploy-your-application-code-to-azure
+
+Write-Host "`nSTEP - Configuring settings for: $appServiceName`n"
+
+az webapp config appsettings set `
+    -g $rgName `
+    --name $appServiceName `
+    --settings SCM_DO_BUILD_DURING_DEPLOYMENT=true
+
+
+# -- Configure settings for the web app. These are available to the app as environment variables.
+
+Write-Host "`nSTEP - Configuring web app settings for: $appServiceName`n"
+
+az webapp config appsettings set `
+    -g $rgName `
+    --name $appServiceName `
+    --settings "FILEUP_SECRET_KEY=$AppSecretKey" `
+    "FILEUP_MAX_UPLOAD_MB=$AppMaxUploadSizeMb" `
+    "FILEUP_DATABASE_URI=$AppDatabaseURI"
+
+
 # -- Create SQL Server.
 #    https://docs.microsoft.com/en-us/cli/azure/sql/server?view=azure-cli-latest#az-sql-server-create
+
+Write-Host "`nSTEP - Creating SQL Server: $sqlServerName`n"
 
 az sql server create --name $sqlServerName -g $rgName `
   --location $location `
@@ -92,13 +148,15 @@ az sql server create --name $sqlServerName -g $rgName `
 #    https://docs.microsoft.com/en-us/azure/app-service/overview-inbound-outbound-ips#find-outbound-ips
 #    https://docs.microsoft.com/en-us/cli/azure/sql/server/firewall-rule?view=azure-cli-latest#az-sql-server-firewall-rule-create
 
+Write-Host "`nSTEP - Adding firewall rules for: $sqlServerName`n"
+
 $ipAddresses = az webapp show -g $rgName --name $webAppName --query outboundIpAddresses --output tsv
 $ipNum = 0
-foreach ($ip in $ipAddresses.Split(",")) 
-{ 
+foreach ($ip in $ipAddresses.Split(","))
+{
   $ipNum += 1
   $ruleName = "WebAppIP$ipNum"
-  Write-Host $ruleName $ip 
+  Write-Host $ruleName $ip
   az sql server firewall-rule create -g $rgName -s $sqlServerName -n $ruleName --start-ip-address $ip --end-ip-address $ip
 }
 
@@ -107,9 +165,13 @@ foreach ($ip in $ipAddresses.Split(","))
 # -- Create SQL Database.
 #    https://docs.microsoft.com/en-us/cli/azure/sql/db?view=azure-cli-latest#az-sql-db-create
 
+Write-Host "`nSTEP - Creating database: $sqlDatabaseName`n"
+
 az sql db create --name $sqlDatabaseName -g $rgName --server $sqlServerName
 
 
+# ----------------------------------------------------------------------
+# Additional commands and information.
 
 # -- Get the connection string.
 # $connStr = $(az sql db show-connection-string -s $sqlServerName -n $sqlDatabaseName -c ado.net)
